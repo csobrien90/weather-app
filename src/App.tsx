244114af105089c30043797ae9486538a5155a36
Chad OBrien <obrien.music@gmail.com>
2024-02-27 15:29:19 -0500

    remove comment

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import { SetStateAction, useState } from 'react';
+import { useState } from 'react';
 
 import LocationInput from './components/LocationInput'
 import Weather from './components/Weather'
 
 import './App.css'
 
-function App() {
+export default function App() {
 	const [locationData, setLocationData] = useState({
 		streetAddress: '',
 		city: '',
@@ -18,21 +18,6 @@ function App() {
 		periods: []
 	});
 
-	/*
-		1. Input and Submit: Provide an input field where users can enter an address, city, or
-		other location and a method for querying weather data based on user input.
-		2. Geolocation: Utilize the Nominatim API to convert the location entered by the user into
-		latitude and longitude coordinates.
-		3. Weather: Fetch weather data from the National Weather Service API using the latitude
-		and longitude coordinates obtained from the geolocation API.
-		4. Display: Display the following weather information for the specified location:
-
-		- Temperature (in Fahrenheit)
-		- Description of the weather conditions
-		- Wind speed
-		- Detailed forecast
-	*/
-	
 	return (
 		<main>
 			<h1>Weather App</h1>
@@ -41,5 +26,3 @@ function App() {
 		</main>
 	)
 }
-
-export default App
